9e1b3afdc916a9eda29399e89ed1a186f65a28a0
Tomas Romančik <88852500+TomasioLT@users.noreply.github.com>
2023-01-04 09:26:21 +0200

    Update Sidebar.jsx

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/user.png`}
+                  src="https://raw.githubusercontent.com/TomasioLT/react-admin/gh-pages/assets/user.png"
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
